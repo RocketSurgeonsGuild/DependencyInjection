d7bc44b80e8159f771e170c2bc64c170cf5edadf
David Driscoll <david.driscoll@gmail.com>
2020-09-04 09:09:45 -0400

    Moved extensions around

--- src/WebAssembly.Hosting.Autofac/AutofacRocketHostExtensions.cs
+++ src/WebAssembly.Hosting.Autofac/AutofacRocketHostExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using Autofac;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Rocket.Surgery.Conventions;
 using Rocket.Surgery.Conventions.Autofac;
@@ -17,23 +19,6 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Hosting
     [PublicAPI]
     public static class WebAssemblyAutofacRocketHostExtensions
     {
-        /// <summary>
-        /// Uses the Autofac.
-        /// </summary>
-        /// <param name="builder">The builder.</param>
-        /// <param name="delegate">The container.</param>
-        /// <returns>IHostBuilder.</returns>
-        public static ConventionContextBuilder ConfigureAutofac([NotNull] this ConventionContextBuilder builder, AutofacConvention @delegate)
-        {
-            if (builder == null)
-            {
-                throw new ArgumentNullException(nameof(builder));
-            }
-
-            builder.AppendDelegate(@delegate);
-            return builder;
-        }
-
         /// <summary>
         /// Uses the autofac.
         /// </summary>
